a82fe1c38725ddcd758f1bfd60577330ea4d14fe
deeptaste <diptesh.shrestha@gmail.com>
2014-05-13 22:27:55 +0100

    Fixing Issues

--- js/index.js
+++
@@ -1,8 +0,0 @@
-function load() {
-    document.addEventListener("deviceready", onDeviceReady, true);
-}
-
-
-var onDeviceReady = function() {
-    document.getElementById('deviceName').innerHTML = device.name;
-};
